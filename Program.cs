6324448ac3590e4d620203eac48412114b399b94
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-14 17:28:47 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        static Program()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program !");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
