86517d06f43525de1ab639090dee31d521e391a8
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 14:51:15 +0530

    adding attendenca

---
+++ Attendance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    
+    class Attendance
+    {
+        public void isPresent()
+        {
+            Random random = new Random();
+            if (random.Next() % 2 == 0)
+            {
+                Console.WriteLine("Employee is 'ABSENT' ");
+            }
+            else
+            { 
+                Console.WriteLine("Employee is 'PRESENT'");
+            }
+        }
+
+    }
+}
