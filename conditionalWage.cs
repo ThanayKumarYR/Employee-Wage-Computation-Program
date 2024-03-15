beaebd155320c19321a5ed75533e051df2bfe31b
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 15:45:51 +0530

    changes of conditional wages and daily wages

--- conditionalWage.cs
+++ conditionalWage.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Employee_Wage_Computation
 {
-    internal class coinditionalWage
+    class conditionalWage
     {
+        private int limit = 12;
+        private int pay;
+        private int noOfDayWork;
+        public conditionalWage(int pay, int noOfDayWork)
+        {
+
+            this.pay = pay;
+            this.noOfDayWork = noOfDayWork;
+        }
+
+        public void decide_pay()
+        {
+            if (limit > noOfDayWork)
+            {
+                Console.WriteLine("pls work for some more day");
+            }
+            else
+            {
+                Console.WriteLine($"amount of salary {(noOfDayWork - limit) * pay}");
+            }
+        }
     }
 }
--- dailywages.cs
+++ dailywages.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Employee_Wage_Computation
 {
-    internal class dailywages
+    class dailywages
     {
+        public static void dailywage()
+        {
+            int wage_per_hour = Convert.ToInt32(Console.ReadLine());
+            int per_day_work = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"the wage per day is {wage_per_hour * per_day_work}");
+
+        }
     }
 }
