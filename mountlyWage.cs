c2a423a2e84e23d07a66a4d4168e870254d07ddc
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 15:11:01 +0530

    adding monthlywages

---
+++ mountlyWage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation
+{
+    class mountlyWage
+    {
+        private int numberOfDay;
+        private int payPerday;
+        public mountlyWage(int numberOfDay, int payPerDay)
+        {
+            this.numberOfDay = numberOfDay;
+            this.payPerday = payPerDay;
+        }
+        public void calculateMountlyWage()
+        {
+            Console.WriteLine($"the mountly wage is {numberOfDay * payPerday}");
+        }
+    }
+}
