1ad8b55444b6882f4d342dcc35898ba69ba04950
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 15:00:14 +0530

    adding partTimeWages

---
+++ partTimeWages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation
+{
+    
+    class partTimeWages
+    {
+        public void ptWages()
+        {
+            int part_time = Convert.ToInt32(Console.ReadLine());
+            int wage_per_hour = 14;
+            Console.WriteLine($"{wage_per_hour * part_time}");
+        }
+
+    }
+}
